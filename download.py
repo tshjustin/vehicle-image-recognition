7cf6dbb9d495fcbb81961342eb05777b57663dd3
Justin <j.tann2305@gmail.com>
2024-02-18 13:59:11 +0800

    added data pipeline

--- download.py
+++ download.py
@@ -1,17 +1,71 @@
+from settings import LTA_TRAFFIC_URL, DOWNLOAD_FOLDER, get_logger
+import requests 
+import time
+import os 
+
 '''
 Queries the Endpoints and obtain Traffic Images each minute 
 '''
-from settings import AUTH_HEADERS, LTA_TRAFFIC_URL
-import requests 
+# Schema of items 
+'''
+[
+    {
+        timestap: str 
+        camera: [ {...} ]
+    }
+    
+]
+'''
 
-def download():
+# Schema of "cameras" field in Items
+'''
+{
+    timestamp=str, 
+    image=str
+    location = {}
+}
+'''
+
+logger = get_logger(__name__)
+
+def download(url: str):
     '''
     Downloads the traffic images from LTA API and store into a Folder 
     
     r type: Returns a Dictionary, with the content inside the Value of 'Value' Key
     '''
-    response = requests.get(LTA_TRAFFIC_URL, headers=AUTH_HEADERS) 
-    print(response.json())
+    img_data = requests.get(url).content # Upon GET to the endpoint, image is downloaded
+    logger.info(f'Downloading: {url}') 
     
+    # Stores the images into a folder 
+    try:
+        if not os.path.exists(DOWNLOAD_FOLDER):
+            os.mkdir(DOWNLOAD_FOLDER)
+
+        with open(f'{DOWNLOAD_FOLDER}/{url.split("/")[-1]}', 'wb') as handler:
+            handler.write(img_data)
+    except Exception as e:
+        logger.error(e)
+    
+def loop(n: int=50):
+    '''
+    Downloads batches of Data from API at different timings for a diverse Dataset 
+    '''
+    count = 1 
+    response = requests.get(LTA_TRAFFIC_URL) 
+    logger.info(f'Response Status: {response.status_code}')
+    items = response.json().get('items',[]) # Returns a list of Dict
+    
+    for item in items: 
+        for camera in item.get('cameras', []):
+            url_download = camera.get('image')
+            
+            if url_download:
+                download(url_download)
+                count +=1 
+                
+                if count > n:
+                    return
+                
 if __name__ == '__main__':
-    download()
+    loop(30)
--- settings.py
+++ settings.py
@@ -1,13 +1,21 @@
 from dotenv import load_dotenv, find_dotenv
 import os 
+import logging 
 
 load_dotenv(find_dotenv()) # Load Env keys 
 
+# Set Logger 
+logging.basicConfig(level=logging.INFO) # Ensure normal working conditions
+
+def get_logger(name):
+    return logging.getLogger(name)
+
 # URLS 
-LTA_TRAFFIC_URL = 'http://datamall2.mytransport.sg/ltaodataservice/Traffic-Imagesv2'
+LTA_TRAFFIC_URL = 'https://api.data.gov.sg/v1/transport/traffic-images'
 
 # General 
 AUTH_HEADERS = {
     "AccountKey": os.getenv('LTA_API_KEY')
 }
 
+DOWNLOAD_FOLDER = 'assests'
