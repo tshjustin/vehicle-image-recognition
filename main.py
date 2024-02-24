f3436b8d886b78e6d13109cc769622f4a7c5a833
Justin <j.tann2305@gmail.com>
2024-02-24 12:36:09 +0800

    added endpoints for inference

--- main.py
+++ main.py
@@ -1,25 +1,50 @@
-import cv2
-import time
+import json
+import logging
+from flask import abort, Flask, request, Response
+from imread_from_url import imread_from_url
+from requests.exceptions import ConnectionError
 
 from yolov8.YOLOv8 import yolov8_detector
 
-IMG_PATH = 'assests/0cab740a-752c-4bde-a15e-8ead129afce0.jpg'
+logger = logging.getLogger('App')
+
+app = Flask(__name__)
+
+def error_(e: str) -> Response:
+    '''
+    Returns standard error for bad url / invalid image 
+    '''
+    return Response(response=json.dumps({'error': e, 'boxes': [], 'classes': [], 'conf': []}), status=400)
+
+@app.route('/inference')
+def inference():
+    """
+    error: str,                // error message if req failed, else None
+    boxes: list[list[float]],  // coords of bounding boxes (top left as origin, (x1 y1 x2 y2)) if success, else [] 
+    classes: list[int],        // class indexes if success, else [] - len(classes) returns the number of objects that are present in the image 
+    conf: list[float]          // confidence levels if success, else []
+    """
+    url = request.args.get('url', default='', type=str)
+
+    if not url:
+        return error_('No URL received')
+
+    try:
+        img = imread_from_url(url)
+        boxes, scores, class_ids = yolov8_detector(img)
+
+    except ConnectionError: # Invalid image URL format - Since images from APIs may have timeout timer 
+        return error_('Invalid image URL')
+    
+    except Exception as e: # Unknown error  
+        return error_(str(e))
+    
+    return Response(response=json.dumps({
+            'error': None, 
+            'boxes': boxes.tolist(), 
+            'classes': class_ids.tolist(), 
+            'conf': scores.tolist()
+        }), status=200)
 
 if __name__ == '__main__':
-    # Read image
-    img = cv2.imread(IMG_PATH)
-
-    # Detect Objects
-    boxes, scores, class_ids = yolov8_detector(img)
-
-    for score, cls in list(zip(scores, class_ids)):
-        print(f'Class {cls} : Score {score}')
-
-    # Draw detections
-    combined_img = yolov8_detector.draw_detections(img)
-    cv2.imwrite(f'{time.time()}.{IMG_PATH.split(".")[-1]}', combined_img)
-
-    # Open image
-    cv2.namedWindow("Detected Objects", cv2.WINDOW_NORMAL)
-    cv2.imshow("Detected Objects", combined_img)
-    cv2.waitKey(0)
+    app.run() 
---
+++ test.py
@@ -0,0 +1,29 @@
+import cv2
+import time
+from yolov8.YOLOv8 import yolov8_detector
+
+'''
+
+For Manual testing of image detection - Outputs an image with annotated boxes on classes, along with associated confidence levels 
+'''
+
+IMG_PATH = 'assests/0cab740a-752c-4bde-a15e-8ead129afce0.jpg'
+
+if __name__ == '__main__':
+    # Read image
+    img = cv2.imread(IMG_PATH)
+
+    # Detect Objects
+    boxes, scores, class_ids = yolov8_detector(img)
+
+    for score, cls in list(zip(scores, class_ids)):
+        print(f'Class {cls} : Score {score}')
+
+    # Draw detections
+    combined_img = yolov8_detector.draw_detections(img)
+    cv2.imwrite(f'{time.time()}.{IMG_PATH.split(".")[-1]}', combined_img)
+
+    # Open image
+    cv2.namedWindow("Detected Objects", cv2.WINDOW_NORMAL)
+    cv2.imshow("Detected Objects", combined_img)
+    cv2.waitKey(0)
